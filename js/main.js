4caea45541468764ed75dee1f0210358ca394459
middtown <aarontm9@gmail.com>
2017-12-12 10:00:25 -0700

    updated spacing in js

--- js/main.js
+++ js/main.js
@@ -1,42 +1,39 @@
 window.onload = function(){
 	console.log("js is working");
-		// grabs current position, starting line
-		var pos2 = 0;
-		var pos = 0;
-		// grabs player one globally
-		var p1 = document.getElementById("player1"); 
-		var p2 = document.getElementById("player2");
+	// grabs current position, starting line
+	var pos2 = 0;
+	var pos = 0;
+	// grabs player one globally
+	var p1 = document.getElementById("player1"); 
+	var p2 = document.getElementById("player2");
+
 document.addEventListener("keydown", function(event) { 
 	var keyName = event.key;
 	console.log(keyName);
+
 	// if statements to create keys to press and race
-if (keyName == "f"){
+	if (keyName == "f"){
 	console.log("player1");
 	pos+=100;
 	console.log(pos);
 	p1.style.marginLeft = pos + "px";
-} else if (keyName == "j") {
+	} else if (keyName == "j") {
 	console.log("player2");
 	pos2+=100;
 	console.log(pos2);
 	p2.style.marginLeft = pos2 + "px";
-}
+	}
+
 	// winner rational
-if ( pos == 900 && pos2 <= 899) {
+	if ( pos == 900 && pos2 <= 899) {
 	console.log("player 1 wins");
 	alert("Player 1 Wins!!");
-} else if (pos2 == 900 && pos <= 899) {
+	} else if (pos2 == 900 && pos <= 899) {
 	console.log("player 2 wins");
 	alert("Player 2 Wins!!");
-}
+	}
 });
-
-
-
-
-
-
-
+// 	---------------			GRAVE YARD 				------------------- //
 // document.getElementById("player1").onkeydown = function letsMove() {
 // 	console.log("keydown");
 // 	
@@ -62,4 +59,5 @@ if ( pos == 900 && pos2 <= 899) {
 // var context = canvas.getContext("2d"); 
 // 	context.fillStyle = "rgb(100, 200, 120)";
 // 	context.fillRect(-50, 10, 55, 50);
+
 };
